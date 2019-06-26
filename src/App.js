8899813c39885f508520a15dde329851004e0c7e
Manoj Fernando <manojf@99x.lk>
2019-06-26 06:30:07 +0530

    Added two endpoints from Amplify and Serverelss

--- src/App.js
+++ src/App.js
@@ -6,15 +6,33 @@ import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
 
+import axios from "axios";
+
+import { API } from "aws-amplify";
+
 Amplify.configure(awsconfig);
 
 class App extends React.Component {
+  callAmplifyApi = () => {
+    API.get("todos", "/items", {});
+  };
+
+  callSlsApi = () => {
+    axios
+      .get("https://lc4l394bej.execute-api.us-east-1.amazonaws.com/dev/todos")
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello World - Chaged!</p>
+          <p>Hello World!</p>
+          <button onClick={this.callAmplifyApi}>Call Amplify Rest API</button>
+          <hr />
+          <button onClick={this.callSlsApi}>Call Serverless Endpoint</button>
         </header>
       </div>
     );
