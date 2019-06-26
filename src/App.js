2f7e8a1ac438785449e491001670c695fc69347c
Manoj Fernando <manojf@99x.lk>
2019-06-26 07:04:14 +0530

    Added two endpoints from Amplify and Serverelss

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
 
   callSlsApi = () => {
     axios
-      .get("https://xai0uuu1yg.execute-api.us-east-1.amazonaws.com/dev/todos")
+      .get("https://a6a7apt6if.execute-api.us-east-1.amazonaws.com/dev/todos")
       .then(res => console.log(res))
       .catch(err => console.log(err));
   };
